1982b75482dffac377595c2abba393ef5f374802
Janv-iii <janvisomvanshi@gmail.com>
2024-07-24 19:20:42 +0530

    py code1

--- first.py
+++ first.py
@@ -1,1 +1,18 @@
-print("hello world")
+#Making basic python calculator
+a=int(input("enter a no. a:"))
+b=int(input("enter a no. b:"))
+operator=input("enter any operator:")
+if(operator=='+'):
+    print(a+b)
+elif(operator=='*'):
+    print(a*b)
+elif(operator=='-'):
+    print(a-b)
+elif(operator=='/'):
+    print(a/b)
+elif(operator=='%'):
+    print(a%b)
+elif(operator=='//'):
+    print(a//b)
+else:
+    print("No such operator found")
