adf5f9fbae792cdb9d3a06142694a06993a04eee
Janv-iii <janvisomvanshi@gmail.com>
2024-07-27 23:59:31 +0530

    code2

---
+++ code2.py
@@ -0,0 +1,5 @@
+#Program to obtain length and breadth of a rectangle and calculate the area.
+length=int(input("enter the length of rectangle:"))
+breadth=int(input("enter the breadth of the rectangle:"))
+area=length*breadth
+print("area of rectangle is:",area)
