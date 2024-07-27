f63a5ddbbcfc517b08c9f61b2db2d746c76fd19f
Janv-iii <janvisomvanshi@gmail.com>
2024-07-28 00:13:36 +0530

    code5

---
+++ code5.py
@@ -0,0 +1,7 @@
+#code to swap 3 no.
+x=int(input("enter number1:"))
+y=int(input("enter number2:"))
+z=int(input("enter number3:"))
+print("Before swapping:",x,y,z)
+x,y,z=z,x,y
+print("After swapping:",x,y,z)
