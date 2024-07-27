cf3971f8b5cd7a972598ac51d13e76f1c9d769fd
Janv-iii <janvisomvanshi@gmail.com>
2024-07-28 00:25:49 +0530

    code6

---
+++ code6.py
@@ -0,0 +1,8 @@
+#Write a code to swap two numbers using a third variable.
+x=int(input("enter a no. x:"))
+y=int(input("enter a n. y:"))
+print("Before swapping:",x,y)
+t=x
+x=y
+y=t
+print("After swapping:",x,y)
