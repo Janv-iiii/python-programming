4f7a4aceb9f698e5c9e80faa23bea520c4e747b0
Janv-iii <janvisomvanshi@gmail.com>
2024-07-27 23:53:27 +0530

    code1

---
+++ code1.py
@@ -0,0 +1,6 @@
+#To obtain three no. and print their sum
+num1=int(input("enter the no. 1:"))
+num2=int(input("enter the no. 2:"))
+num3=int(input("enter the no. 3:"))
+sum_=num1+num2+num3
+print("sum of three no. is:",sum_)
