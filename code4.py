1edbb50443268f45d0a60c8feb35591df1463cbf
Janv-iii <janvisomvanshi@gmail.com>
2024-07-28 00:08:43 +0530

    code4

---
+++ code4.py
@@ -0,0 +1,6 @@
+#write a code to input two no. and swap them
+n1=int(input("enter a no. n1:"))
+n2=int(input("enter a no. n2:"))
+print("Before swapping:",n1,n2)
+n1,n2=n2,n1
+print("After swapping:",n1,n2)
